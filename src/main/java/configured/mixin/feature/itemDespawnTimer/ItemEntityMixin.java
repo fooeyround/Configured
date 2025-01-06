f6166a1b0b12d22149789d3267e306eeac5d2ce3
fooeyround <DevJackMC@gmail.com>
2025-01-05 21:49:31 -0800

    bump version (1.4.2): Fix some parts of item despawn behavior not being respected

--- src/main/java/configured/mixin/feature/itemDespawnTimer/ItemEntityMixin.java
+++ src/main/java/configured/mixin/feature/itemDespawnTimer/ItemEntityMixin.java
@@ -10,9 +10,23 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin {
 
-    @ModifyConstant(method = "tick", constant = @Constant(intValue = 6000))
+    @ModifyConstant(method = {"tick", "canMerge()Z"}, constant = @Constant(intValue = 6000))
     private int configured$ItemDespawnAge(int def) {
         return Settings.itemDespawnAge;
     }
+
+    @ModifyConstant(method = "setDespawnImmediately", constant = @Constant(intValue = 5999))
+    private int configured$ItemDespawnAge$setDespawnImmediately(int def) {
+        return Settings.itemDespawnAge-1;
+    }
+
+
+    @ModifyConstant(method = "setCovetedItem", constant = @Constant(intValue = -6000))
+    private int configured$setCov(int def) {
+        return -Settings.itemDespawnAge;
+    }
+
+
+
 
 }
